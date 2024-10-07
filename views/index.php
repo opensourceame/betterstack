4b9321cb3900bb2fe0849c1fd73ba621a72e1575
David Kelly <david@futuresbright.com>
2024-10-07 19:01:52 +0200

    move form and change URL for heroku

--- views/index.php
+++ views/index.php
@@ -11,34 +11,7 @@
 
 <div id='errors_container'></div>
 
-<form id="new_user_form" method="post" action="" class="form-horizontal">
-	<div class="form-group">
-		<label for="name" class="col-sm-4 control-label">Name:</label>
-		<div class="col-sm-8">
-			<input name="name" type="text" id="name" class="form-control" value="<?= $params['name'] ?>" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="email" class="col-sm-4 control-label">E-mail:</label>
-		<div class="col-sm-8">
-			<input name="email" type="text" id="email" class="form-control" value="<?= $params['email'] ?>" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="city" class="col-sm-4 control-label">City:</label>
-		<div class="col-sm-8">
-			<input name="city" type="text" id="city" class="form-control" value="<?= $params['city'] ?>" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="phone_number" class="col-sm-4 control-label">Phone Number:</label>
-		<div class="col-sm-8">
-			<input name="phone_number" type="text" id="phone_number" class="form-control" value="<?= $params['phone_number'] ?>" />
-		</div>
-	</div>
-
-	<button id='new_user_submit_btn' type="submit" class="btn btn-primary">Create new row</button>
-</form>
+<?php include('form.php'); ?>
 
 <script type='text/javascript'>
 	function submitForm() {
@@ -48,7 +21,7 @@
 
 		$.ajax({
                     type: "POST",
-                    url: '/create',
+                    url: '/index.php',
                     data: form.serialize(), // Serialize form data
                     success: function (data) {
 						$('#users_container')[0].innerHTML = data;
